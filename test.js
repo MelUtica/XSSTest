3fd0a6b6782417965e64a0d62e8e4a0ced89b4b3
MelUtica <36487391+MelUtica@users.noreply.github.com>
2018-02-14 14:32:54 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,3 @@
-<!DOCTYPE HTML>
-<html>
-
-<body>
-
-  <p>Before the script...</p>
-
   <script>
     alert( 'You have been hacked by Chris! Hahaha!' );
   </script>
-
-  <p>...After the script.</p>
-
-</body>
-
-</html>
